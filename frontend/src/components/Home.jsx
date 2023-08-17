587e35082d1b671c25849d6df0034c7e16e6bd80
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-08-17 17:28:45 -0500

    pagination feature depending on result per page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../actions/productActions'
 import Product from './product/product'
 import Loader from './layout/loader';
-import {useAlert} from 'react-alert';
+import { useAlert } from 'react-alert';
 import Pagination from 'react-js-pagination'
 
 const Home = () => {
@@ -12,45 +12,46 @@ const Home = () => {
     const [currentPage, setCurrentPage] = useState(1)
 
     const setCurrentPageNo = (pageNumber) => {
-            setCurrentPage(pageNumber)
+        setCurrentPage(pageNumber)
     }
 
     const alert = useAlert();
     const dispatch = useDispatch()
 
-    const {loading, error, products, productsCount, resPerPage} = useSelector(state => state.products)
+    const { loading, error, products, productsCount, resPerPage } = useSelector(state => state.products)
 
     useEffect(() => {
-        if (error){ 
+        if (error) {
             return alert.error(error)
         }
         dispatch(getProducts(currentPage));
 
     }, [dispatch, error, alert, currentPage])
-    
-  return (
-      <Fragment>
-          {loading ? <Loader/> : (
-              <Fragment>
-                  <MetaData title='All Products' />
-                  <h1 id='products_heading'>Latest Products</h1>
-                  <section id="products" className="container mt-5">
-                      <div className="row">
-                          {products && products.map(product => (
-                              <Product key={product._id} product={product} />
-                          ))}
+
+    return (
+        <Fragment>
+            {loading ? <Loader /> : (
+                <Fragment>
+                    <MetaData title='All Products' />
+                    <h1 id='products_heading'>Latest Products</h1>
+                    <section id="products" className="container mt-5">
+                        <div className="row">
+                            {products && products.map(product => (
+                                <Product key={product._id} product={product} />
+                            ))}
 
 
 
 
-                      </div>
-                  </section>
+                        </div>
+                    </section>
 
-                  <div className="d-flex justify-content-center mt-5" >
+                    {resPerPage < productsCount && (
+                        <div className="d-flex justify-content-center mt-5" >
                             <Pagination
                                 activePage={currentPage}
                                 itemsCountPerPage={resPerPage}
-                                totalItemsCount = {productsCount}
+                                totalItemsCount={productsCount}
                                 onChange={setCurrentPageNo}
                                 nextPageText={'Next'}
                                 prevPageText={'Prev'}
@@ -60,12 +61,15 @@ const Home = () => {
                                 linkClass='page-link'
                             />
 
-                  </div>
-              </Fragment>
-          )}
+                        </div>
+                    )}
 
-      </Fragment>
-  )
+
+                </Fragment>
+            )}
+
+        </Fragment>
+    )
 }
 
 export default Home
