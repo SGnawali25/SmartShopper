4adad811b0594cccaceaade567af65061d985480
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-09-28 01:57:28 -0500

    cors management

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(fileUpload());
 
 
 app.use("*",cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://smartshopperapi.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
