31c1a4d430fc302dfcbc8bc860485493ea0b92d1
Sandesh Gnawali <sandeshgyawali25@gmail.com>
2023-09-28 01:32:42 -0500

    update in cors

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,8 @@ app.use(fileUpload());
 
 
 app.use("*",cors({
-    origin: true,
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }));
 
